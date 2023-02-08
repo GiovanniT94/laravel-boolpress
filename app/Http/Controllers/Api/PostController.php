0968110a421bb0fff985b72fee2bcc0f229b2687
GiovanniT94 <giovanni.t94@hotmail.it>
2023-02-08 10:56:18 +0100

    Es completo

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+
+    public function index()
+    {
+        $posts = Post::paginate(6);
+
+        return response()->json([
+            'success' => true,
+            'results' => $posts
+        ]);
+    }
+
+
+    public function show(Post $post)
+    {
+        $post = Post::where('id', $post->id)->with(['category', 'tags'])->first();
+
+        return response()->json([
+            'success' => true,
+            'results' => $post
+        ]);
+    }
+
+    public function random() {
+        $posts = Post::inRandomOrder()->limit(9)->get();
+
+        return response()->json([
+            'success' => true,
+            'results' => $posts,
+        ]);
+    }
+}
+
---
+++ resources/js/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <section>
+        <div class="text-center">Sono il Footer</div>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
